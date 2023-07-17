504a37f565401d6c967405ccc49b81b9141e2fef
xuog <132880568+xuog@users.noreply.github.com>
2023-07-17 21:49:29 +0700

    Update update.php

--- category/update.php
+++ category/update.php
@@ -1,14 +1,10 @@
 <?php
 require_once 'pdo.php';
-require_once 'helper.php';
-
 $request = $_POST;
-
 $category = [
-    'name' =>$request['name'],
-    'id'=>$request['id']
+    'name' => $request['name'],
+    'id' => $request['id'],
 ];
-
-update($category);
-redirectHome();
-?>
+$categoryConnection= new CategoryConnection();
+$getData->update($category);
+header("Location: http://localhost/hoc.php/product/index.php");
