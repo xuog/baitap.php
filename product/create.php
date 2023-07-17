045adf2a9c72c98e09a9c6e9aa8c1a0067337a19
xuog <132880568+xuog@users.noreply.github.com>
2023-07-17 21:52:47 +0700

    Update create.php

--- product/create.php
+++ product/create.php
@@ -1,7 +1,8 @@
 <?php
     require_once "pdo.php";
     require_once "../category/pdo.php";
-    $cate = getData();
+    $categoryConnection = new CategoryConnection();
+$cate = $categoryConnection->getData();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,32 +11,23 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-3">
-        <a href="index.php" class="btn" style="margin-right: 5px"> < Back</a>
-        <h3>Create New Product</h3>
-        <form action="action-create.php" method="POST">
+<div class="container mt-3">
+    <div class="container-fluid"><h3>Create Product</h3></div>
+    <a href="./index.php" class="btn btn-primary">Back</a>
+    <form method="POST" action="./store.php">
         <div class="mb-3">
-            <label class="form-label">Name</label>
-            <input required type="text" class="form-control" name="name" placeholder="Enter name...">
+            <label for="" class="form-label">Name</label>
+            <input required type="text" class="form-control" name="name" placeholder="Enter name ...">
+            <label for="" class="form-label">Price</label>
+            <input required type="text" class="form-control" name="price" placeholder="Enter name ...">
+            <label for="" class="form-label">Category_id</label>
+            <input required type="text" class="form-control" name="ca_id" placeholder="Enter name ...">
         </div>
-        <div class="mb-3">
-            <label class="form-label">Price</label>
-            <input required type="text" class="form-control" name="price" placeholder="Enter price...">
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Category</label>
-            <select class="form-select" aria-label="Default select example" name="cateId">
-                <?php 
-                    foreach($cate as $value):
-                ?>
-                <option value="<?=$value['id']?>"><?= $value['name'] ?></option>
-                <?php
-                    endforeach;
-                ?>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-success">Create</button>
-        </form>
-    </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
