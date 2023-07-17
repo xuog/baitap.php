30599eaf77ea495c1f0a32c8ec8abe31878ae560
xuog <132880568+xuog@users.noreply.github.com>
2023-07-17 21:36:34 +0700

    Update edit.php

--- category/edit.php
+++ category/edit.php
@@ -1,37 +1,29 @@
 <?php
     require_once 'pdo.php';
-
-    $category = findById($_GET["id"]);
-    var_dump($category);
 ?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
+    <title>Chỉnh sửa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
 </head>
 <body>
-<div class="container mt-3">
-    <div class="container-fluid"><h3>Update Category</h3></div>
-    <a href="./index.php" class="btn btn-primary">Back</a>
-    <form method="POST" action="./update.php">
-        <input type="hidden" name="id" value="<?php echo $category ['id'];?>">
+    <div class="container mt-3">
+        <h3>Update Category</h3>
+        <?php 
+       $categoryConnection = new CategoryConnection();
+            $data = [
+                'id' => $_GET['id']
+            ];
+            $value = $categoryConnection->getOneData($data)['0'];
+        ?>
+        <form action="./action-edit.php?id=<?=$data['id'] ?>" method="POST">
         <div class="mb-3">
-            <label for="" class="form-label">Name</label>
-            <input required type="text" class="form-control" name="name" placeholder="Enter name ..." value="<?= $category ['name'];?>">
+            <label class="form-label">Name</label>
+            <input type="text" class="form-control" name="name" value="<?= $value['name']; ?>">
         </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-    </form>
-</div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <button type="submit" class="btn btn-success">Update</button>
+        </form>
+    </div>
 </body>
 </html>
