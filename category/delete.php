861fbb93b5a5b50fc747f33f3153c0f705185f1e
xuog <132880568+xuog@users.noreply.github.com>
2023-07-17 21:33:43 +0700

    Update delete.php

--- category/delete.php
+++ category/delete.php
@@ -1,10 +1,9 @@
 <?php
 
 require_once 'pdo.php';
-require_once 'helper.php';
 
 if ($_POST['id'] > 0 && is_numeric($_POST['id'])) {
     delete(['id' => $_POST['id']]);
 }
-
-redirectHome();
+header("Location: http://localhost/hoc.php/product/index.php");
+?>
