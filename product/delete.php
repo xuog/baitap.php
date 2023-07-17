c45af8fb810f2e5dc8e12eebf8c70e29b51a9a83
xuog <132880568+xuog@users.noreply.github.com>
2023-07-17 21:53:19 +0700

    Update delete.php

--- product/delete.php
+++ product/delete.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'pdo.php';
+    $productConnection = new ProductConnection();
     $id = ['id' => $_POST['id']];
-    deleteProdData($id);
-    header("Location: http://localhost/learn_php/product/index.php");
+    $productConnection->deleteProdData($id);
+    header("Location: http://localhost/hoc.php/product/index.php");
 ?>
